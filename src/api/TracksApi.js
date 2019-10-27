b2c95553fb2c534d26ce4c0ca2196c4e6e853ced
Kallebe Felipe <kallebefelipe@gmail.com>
2019-10-27 02:53:53 -0300

    Update token track api

--- src/api/TracksApi.js
+++ src/api/TracksApi.js
@@ -4,7 +4,7 @@ class TracksApi {
     static getAllTracks(id) {
       return fetch(`https://api.spotify.com/v1/albums/${id}/tracks`, {
         headers: {
-          'Authorization': 'Bearer BQB8TgdiUVKR-jFIN4djdXPU9KoksqXUcF0dPMiAzHox1bWbbFdApIfuVE79qV63gHwLV8tZlHA7mxoH6YNFJQg-FED0n9IKhiZBeAhCNv9PWGLV7tZjrDHyGlHn8YmuFVlVOOSSQ6f_d-dLHaSXnQuZ936e2McxnVe0pOO_cvnXu0m6sDZ6bMwBoo4_ZEgF3-zEiQ0ygnBZYz5x4LWX7CC2u_sMjXzz3w8npieUpIYGIb4Aw696uoYJZlz7guu-QQshI5lvp8s6ObnYaGz_56g3rmaIypZU',
+          'Authorization': 'Bearer BQAaSM5tt3b5D3ihDt-zSWE3GEQ7UJPlzi-uAdIgxxIRia3bvKAxiuJ_Y-2pX3BHocWYjMI45BtFa5r03NEe6j4MH4nyCgmbg4ofojl5oCajjIwyl1PSQqqh4FF98uboZKw52fTvQ8YMglOdM988hJc1H2VM_pkh8MlCATWZIlHg5A6-ls_aUeoUYTNMjYmWhG_WDWft3qPk7mLATupQYsB2WXp5M7n1eMl44I42dZO9PJK4qQ7k115QkeDbnbPxePrGy3p-VRNxR9aIOiR5cqTLuAwZxRtf',
           'Content-Type': 'application/json',
           'Accept': 'application/json'
           },
