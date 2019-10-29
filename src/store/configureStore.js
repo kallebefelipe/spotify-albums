675c14b9736d912889983faf5cfc8ed598980ae1
Kallebe Felipe <kallebefelipe@gmail.com>
2019-10-29 20:42:37 -0300

    Configure reducer album

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,6 +2,7 @@ import "regenerator-runtime/runtime";
 import {createStore, combineReducers, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import albumsReducer from '../reducers/albums';
+import albumReducer from '../reducers/album';
 import tracksReducer from '../reducers/tracks';
 import authReducer from '../reducers/auth';
 import root from '../sagas';
@@ -12,6 +13,7 @@ const setUpStore = () => {
   const store = createStore(
     combineReducers({
       albumsReducer,
+      albumReducer,
       tracksReducer,
       authReducer
     }),
